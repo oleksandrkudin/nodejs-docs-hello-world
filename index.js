7b34ef33f6978107dbe79f5895b5cb9ce79d3f90
oleksandrkudin <oleksandr.kudin@gmail.com>
2019-07-19 09:53:57 +0300

    Changes output message 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Oleksandr! Webhooks work ...");
+    response.end("Good job Oleksandr! New commit to application master branch on Gitbug triggers jenkins job what create Docker container with updated application!!!");
 });
 
 const port = process.env.PORT || 1337;
