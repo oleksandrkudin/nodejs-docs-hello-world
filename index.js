19676f9aff88b5b53ba6c3bdcdece013772d793f
oleksandrkudin <oleksandr.kudin@gmail.com>
2019-07-19 10:01:19 +0300

    Fixes errors + adding details to output message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Good job Oleksandr! New commit to application master branch on Gitbug triggers jenkins job what create Docker container with updated application!!!");
+    response.end("Good job Oleksandr!\nNew commit to application master branch on Github triggers jenkins job what create version based Docker container with updated application and run it!!!");
 });
 
 const port = process.env.PORT || 1337;
