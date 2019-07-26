07ee7fccd0f4390c28999a1def62c160227cff3e
oleksandrkudin <oleksandr.kudin@gmail.com>
2019-07-26 08:28:58 +0300

    Deploys to Azure Web App

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Good job Oleksandr!\nNew commit to application master branch on Github triggers jenkins job what create version based Docker container with updated application and run it!!!");
+    response.end("Good job Oleksandr!\nNew commit to application master branch on Github triggers deployment to Azure Web App!!!");
 });
 
 const port = process.env.PORT || 1337;
